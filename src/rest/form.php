643a7f31bb9035b034f92d7eec2b8b45ac6292d8
陈家辉 <chenjiahui@haolianluo.com>
2018-06-13 16:03:33 +0800

    feat(src): 去除不需要的文件

--- src/rest/form.php
+++ src/rest/form.php
@@ -7,9 +7,5 @@ echo $form->field($generator, 'modelClass');
 echo $form->field($generator, 'searchModelClass');
 echo $form->field($generator, 'controllerClass');
 echo $form->field($generator, 'baseControllerClass');
-echo $form->field($generator, 'indexWidgetType')->dropDownList([
-    'grid' => 'GridView',
-    'list' => 'ListView',
-]);
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
